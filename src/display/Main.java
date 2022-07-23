bf9be1810d6d3b943fb2e4391fd4de189b4b23d5
kingssik <hyunsikkang@gmail.com>
2022-07-24 00:36:15 +0900

    게시글 작성기능 구현

--- src/display/Main.java
+++ src/display/Main.java
@@ -8,12 +8,23 @@ public class Main {
 		System.out.println("====프로그램 시작====");
 		
 		Scanner sc = new Scanner(System.in);
-
+		int lastArticleId = 0;
+		
 		while (true) {
 			System.out.print("명령어 ) ");
 
 			String cmd = sc.nextLine();
-			
+			if(cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+			} else if(cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			}
 			if(cmd.equals("exit")) {
 				break;
 			}
